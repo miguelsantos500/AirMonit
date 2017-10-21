1057f03c0b7b9283435e6aa496d78e54773bc23a
Luis Barreiro <2161323@my.ipleiria.pt>
2017-10-21 17:22:14 +0100

    Receive Entries Save XML

--- AirMonit/AirMonit_DU/Program.cs
+++ AirMonit/AirMonit_DU/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 namespace AirMonit_DU
 {
     class Program
@@ -16,10 +17,32 @@ namespace AirMonit_DU
 
         private static void Anymethod(string message)
         {
+            XmlDocument doc = new XmlDocument();
             string[] words = message.Split(';');
+            XmlNode papa = doc.CreateNode("element", "AirEntry", "");
+            doc.AppendChild(papa);
+            XmlNode ceIdValue = doc.CreateNode("element", "CityElementID","");
+            ceIdValue.InnerText = words[0];
+            papa.AppendChild(ceIdValue);
+            XmlNode element = doc.CreateNode("element", "Element", "");
+            element.InnerText = words[1];
+            papa.AppendChild(element);
+            XmlNode elemtValue = doc.CreateNode("element", "ElementValue", "");
+            elemtValue.InnerText = words[2];
+            papa.AppendChild(elemtValue);
+            XmlNode date = doc.CreateNode("element", "date", "");
+            date.InnerText = words[3];
+            papa.AppendChild(date);
+            XmlNode city = doc.CreateNode("element", "city", "");
+            city.InnerText = words[4];
+            papa.AppendChild(city);
+            doc.Save(AppDomain.CurrentDomain.BaseDirectory+"teste.xml");
+            Console.WriteLine("Received Entry:");
+            Console.Write("CityElementID:  " + words[0] + "\t Element:  " + words[1] + "\t ElementValue:  " + words[2] +
+                "\t date:  " + words[3] + "\t city:  " + words[4]+"\n");
+            {
 
-            foreach (string str in words)
-                Console.WriteLine(str);
+            }
         }
     }
 }
