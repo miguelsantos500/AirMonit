d55ada965342b1fff38345429c12a9c250fc9bfc
Luis Barreiro <2161323@my.ipleiria.pt>
2017-10-21 16:42:24 +0100

    Get Values

--- AirMonit/AirMonit_DU/Program.cs
+++ AirMonit/AirMonit_DU/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace AirMonit_DU
 {
     class Program
     {
         static void Main(string[] args)
         {
+            AirSensorNodeDll.AirSensorNodeDll  dll= new AirSensorNodeDll.AirSensorNodeDll();
+            dll.Initialize(Anymethod, 2000);
+
+        }
+
+        private static void Anymethod(string message)
+        {
+            string[] words = message.Split(';');
+
+            foreach (string str in words)
+                Console.WriteLine(str);
         }
     }
 }
