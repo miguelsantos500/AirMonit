d267ae3f6ad00f27733d61b46cdf0ee2343c81e5
Soraia <soraiapratesbasso@gmail.com>
2017-10-28 18:42:11 +0100

    Added DuController into package /AirMonitD_Log/Controllers - methods are not complete.

---
+++ AirMonit/AirMonitD_Log/Controllers/DuController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AirMonitD_Log.Controllers
+{
+    public class DuController : ApiController
+    {
+
+        //database connection
+        string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Soraia\source\repos\AirMonit\AirMonit\AirMonitD_Log\App_Data\SqlDatabase.mdf;Integrated Security=True";
+
+       //metodos para controlador gerir a bd
+       //TODO
+       // mudar os return values depois de completar os metodos
+       public void GetAllElements()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM ElementsTable ORDER BY Id", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+        }
+
+        public void GetAllAlarms()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("SELECT * FROM ElementsTable ORDER BY Id", connection);
+
+            SqlDataReader reader = command.ExecuteReader();
+        }
+
+        public void GetDataEntry()
+        {
+
+        }
+
+        public void PutDataEntriesOnDB()
+        {
+
+        }
+    }
+}
